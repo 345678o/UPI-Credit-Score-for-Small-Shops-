24353f3d0b6f3d88ad7e14b548d81f9552695116
ANAMIKA KUMARI <24r21a67d2@mlrit.ac.in>
2026-04-10 20:13:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1827053136-a879b",
+  "appId": "1:239727315860:web:3f5ab9d40333acdaa39d81",
+  "apiKey": "AIzaSyCRowiK03asucdiE_rwXUn9tuUxv0V-0FQ",
+  "authDomain": "studio-1827053136-a879b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "239727315860"
+};
